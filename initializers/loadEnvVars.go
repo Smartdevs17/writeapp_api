dddd979f1c633a9febafe3e9060497aa8388a979
smartdev <smartdevs17@gmail.com>
2024-06-25 13:11:49 +0100

    add go env

--- initializers/loadEnvVars.go
+++ initializers/loadEnvVars.go
@@ -2,13 +2,18 @@ package initializers
 
 import (
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 )
 
 func LoadEnvVars() {
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Fatal("Error loading .env file")
+	env := os.Getenv("GO_ENV")
+	if env == "development" {
+		err := godotenv.Load(".env")
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
 	}
+	log.Printf("Environment: %s", env)
 }
